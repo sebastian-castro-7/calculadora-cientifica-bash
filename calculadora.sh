34532b6c26e903e9b24ee84075e3873e119de6a5
sebastian-castro7 <sebastian@Debian-Laboratorio.myguest.virtualbox.org>
2026-08-19 19:55:12 -0600

    validaciones agregadas

--- calculadora.sh
+++ calculadora.sh
@@ -26,26 +26,88 @@ case $opcion in
 
     case $opcion_basica in
         1)
-            read -p "Ingresa el primer número: " n1
-            read -p "Ingresa el segundo número: " n2
+	    while true; do
+		read -p "ingresa el primer numero: " n1
+		if [[ "$n1" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+		break;
+		else
+		echo "Error: solo se permiten numeros"
+		fi
+		done
+	   while true; do
+		read -p "ingresa el segundo numero: " n2
+		if [[ "$n2" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+		break;
+		else
+		echo "Error: solo se permiten numeros"
+		fi
+		done
+
             resultado=$(echo "scale=6; $n1 + $n2" | bc -l)
             echo "Resultado: $n1 + $n2 = $resultado"
             ;;
         2)
-            read -p "Ingresa el primer número: " n1
-            read -p "Ingresa el segundo número: " n2
+
+      	while true; do
+                read -p "ingresa el primer numero: " n1
+                if [[ "$n1" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
+        while true; do
+                read -p "ingresa el segundo numero: " n2
+                if [[ "$n2" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
+
             resultado=$(echo "scale=6; $n1 - $n2" | bc -l)
             echo "Resultado: $n1 - $n2 = $resultado"
             ;;
         3)
-            read -p "Ingresa el primer número: " n1
-            read -p "Ingresa el segundo número: " n2
+
+	  while true; do
+                read -p "ingresa el primer numero: " n1
+                if [[ "$n1" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
+           while true; do
+                read -p "ingresa el segundo numero: " n2
+                if [[ "$n2" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
+
             resultado=$(echo "scale=6; $n1 * $n2" | bc -l)
             echo "Resultado: $n1 * $n2 = $resultado"
             ;;
         4)
-            read -p "Ingresa el primer número: " n1
-            read -p "Ingresa el segundo número: " n2
+	   while true; do
+                read -p "ingresa el primer numero: " n1
+                if [[ "$n1" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
+           while true; do
+                read -p "ingresa el segundo numero: " n2
+                if [[ "$n2" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
+
             if [ $(echo "$n2 == 0" | bc -l) -eq 1 ]; then
                 echo "Error: No se puede dividir entre cero."
             else
@@ -58,9 +120,6 @@ case $opcion in
             ;;
     esac
 ;;
-     
-
-
 
 
 2)
@@ -70,7 +129,15 @@ echo "1. Seno"
 echo "2. Coseno"
 echo "3. Tangente"
 read -p "Elige una opcion (1-3): " op_trig
-read -p "Ingrese el angulo en radianes: " angulo
+
+	        while true; do
+                read -p "ingresa el numero en radianes: " angulo
+                if [[ "$angulo" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
 
 	case $op_trig in
 	1)
@@ -105,14 +172,38 @@ read -p "Elige una opcion (1-2): " op_exp
 case $op_exp in
 
 	1)
-	read -p "ingresa el numero base: " base
-	read -p "ingresa el exponente (debe ser entero): " exp
+	   while true; do
+                read -p "ingresa el numero base: " base
+                if [[ "$base" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
+
+	   while true; do
+                read -p "ingresa el exponente (debe ser entero): " exp
+                if [[ "$exp" =~ ^-?[0-9]+$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros enteros"
+                fi
+                done
+
 	resultado=$(echo "$base ^ $exp" | bc -l)
 	echo "el resultado de $base elevado a $exp es: $resultado"
 	;;
 
 	2)
-	read -p "ingresa el valor de x para e^x: " valor_e
+	    while true; do
+                read -p "ingresa el valor de x para e^x: " valor_e
+                if [[ "$valor_e" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
+
 	resultado=$(echo "e($valor_e)" | bc -l)
 	echo "El resultado de e^$valor_e es: $resultado"
 	;;
@@ -133,7 +224,15 @@ read -p "Elige una opción: " opcion_trig
 
 case $opcion_trig in
     1)
-        read -p "Ingresa un número entre -1 y 1: " x
+          while true; do
+                read -p "ingresa un numero entre -1 y 1x: " x
+                if [[ "$x" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
+
         if [ $(echo "$x <= -1 || $x >= 1" | bc -l) -eq 1 ]; then
             echo "Error: El dominio de asin es entre -1 y 1."
         else
@@ -142,7 +241,16 @@ case $opcion_trig in
         fi
         ;;
     2)
-        read -p "Ingresa un número entre -1 y 1: " x
+
+	  while true; do
+                read -p "ingresa un numero entre -1 y 1x: " x
+                if [[ "$x" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
+
         if [ $(echo "$x <= -1 || $x >= 1" | bc -l) -eq 1 ]; then
             echo "Error: El dominio de acos es entre -1 y 1."
         else
@@ -151,7 +259,15 @@ case $opcion_trig in
         fi
         ;;
     3)
-        read -p "Ingresa un número: " x
+          while true; do
+                read -p "ingresa un numero entre: " x
+                if [[ "$x" =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+                break;
+                else
+                echo "Error: solo se permiten numeros"
+                fi
+                done
+
         resultado=$(echo "scale=6; a($x)" | bc -l)
         echo "Resultado: atan($x) = $resultado rad"
         ;;
@@ -172,12 +288,38 @@ echo "3. Salir al menu principal"
 read -p "Elige una opcion (1-3): " op_log
 case $op_log in 
 1)
-read -p "Ingrese el numero:" numero
+
+while true; do
+  read -p "ingrese el numero:" numero
+  if [[ "$numero" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+  if [[ "$numero" == "0" || "$numero" == "0.0" ]]; then
+  echo "Error: el logaritmo de cero no existe"
+  else
+  break
+  fi
+  else 
+  echo "Error: solo se permiten numeros positivos"
+  fi
+  done
+
 resultado=$(echo "l($numero)" | bc -l)
 echo "El logaritmo natural es: $resultado"
 ;;
 2)
-read -p "Ingrese el numero:" numero
+
+while true; do
+  read -p "ingrese el numero:" numero
+  if [[ "$numero" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+  if [[ "$numero" == "0" || "$numero" == "0.0" ]]; then
+  echo "Error: el logaritmo de cero no existe"
+  else
+  break
+  fi
+  else 
+  echo "Error: solo se permiten numeros positivos"
+  fi
+  done
+
 resultado=$(echo "l($numero)/l(10)" | bc -l)
 echo "El logaritmo base 10 es: $resultado"
 ;;
@@ -201,13 +343,46 @@ echo "3. Salir al menu principal"
 read -p "Elige una opcion (1-3):" op_raiz
 case $op_raiz in
 1)
+
+while true; do
 read -p "Ingrese el numero:" numero
+ if [[ "$numero" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+ break
+ else 
+ echo "Error: solo se permiten numeros positivos o cero"
+ fi
+ done
+ 
 resultado=$(echo "sqrt($numero)" | bc -l)
 echo " La raiz cuadrada es:$resultado"
 ;;
 2)
+
+while true; do
 read -p "Ingrese el numero:" numero
+if [[ "$numero" =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+ if [[ "$numero" == "0" || "$numero" == "0.0" ]]; then
+   echo "Error: por el metodo logaritmico, el numero debe ser mayor a cero"
+  else
+    break;
+  fi
+   else
+    echo "Error: solo se permiten numeros positivos"
+  fi
+ done
+
+while true; do
 read -p "Ingrese el n de la raiz:" n
+ if [[  "$n" =~ ^-?[0-9]+$ ]]; then
+  if [[ "$n" == "0" ]]; then
+  echo "Error: el indice de la raiz no puede ser cero"
+  else
+    break
+  fi
+    else
+  echo "Error: solo permiten numeros enteros para la n"
+    fi
+  done
 resultado=$(echo "e(l($numero)/$n)" |bc -l)
 echo "La raiz $n-esima es:$resultado"
 ;;
